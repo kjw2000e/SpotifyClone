49a1c82f8413b41bf592a8cd36956b8d05ff684b
jiwon <kjw2000e@2beone.net>
2024-02-21 11:41:25 +0900

    part4 - MusicService 추가, mediaSession, mediaSessionConnector, job , CoroutineScope

--- app/src/main/java/com/plcoding/spotifycloneyt/Exoplayer/MusicService.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/Exoplayer/MusicService.kt
@@ -1,19 +1,64 @@
 package com.plcoding.spotifycloneyt.Exoplayer
 
+import android.app.PendingIntent
+import android.content.Intent
 import android.media.browse.MediaBrowser
+import android.media.session.MediaSession
 import android.os.Bundle
 import android.service.media.MediaBrowserService
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
-import dagger.hilt.android.scopes.ServiceScoped
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 import javax.inject.Inject
 
-@ServiceScoped
-class MusicService : MediaBrowserService() {
+
+private const val SERVICE_TAG = "musicService"
+@AndroidEntryPoint
+class MusicService : MediaBrowserServiceCompat() {
+
+    @Inject
+    lateinit var dataSourceFactory: DefaultDataSourceFactory
+
     @Inject
     lateinit var exoPlayer: SimpleExoPlayer
 
+    // firestroe db에 접근해서 data를 가져오기 위해 코루틴 job을 사용. 서비스 종료시 코루틴 작업을 cancel 시켜야함
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+
     override fun onCreate() {
         super.onCreate()
+        // 필수 mediasession, mediasession connector 생성
+
+        // 알림 발생 후 알림 클릭 시 해당 액티비티로 이동하기 위한 변수
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0 , it, 0)
+        }
+
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
+            setSessionActivity(activityIntent)
+            isActive = true
+        }
+
+        sessionToken = mediaSession.sessionToken
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlayer(exoPlayer)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
     }
 
     override fun onGetRoot(
@@ -26,7 +71,7 @@ class MusicService : MediaBrowserService() {
 
     override fun onLoadChildren(
         parentId: String,
-        result: Result<MutableList<MediaBrowser.MediaItem>>
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
         TODO("Not yet implemented")
     }
