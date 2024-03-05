77938ff4313ba52454e3e63dcb6cec3fe739d9d5
jiwon <kjw2000e@2beone.net>
2024-03-05 15:15:40 +0900

    Songfragment에서 - 재생중인 곡 시간정보 표시 및 seekbar 적용 - prev, next  image 클릭 시 해당 기능 적용

--- app/src/main/java/com/plcoding/spotifycloneyt/ui/fragment/SongFragment.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/ui/fragment/SongFragment.kt
@@ -1,11 +1,14 @@
 package com.plcoding.spotifycloneyt.ui.fragment
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
+import com.plcoding.spotifycloneyt.Exoplayer.isPlaying
 import com.plcoding.spotifycloneyt.Exoplayer.toSong
 import com.plcoding.spotifycloneyt.R
 import com.plcoding.spotifycloneyt.data.entities.Song
@@ -14,8 +17,16 @@ import com.plcoding.spotifycloneyt.other.Status.*
 import com.plcoding.spotifycloneyt.ui.viewmodels.MainViewModel
 import com.plcoding.spotifycloneyt.ui.viewmodels.SongViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_song.ivPlayPauseDetail
+import kotlinx.android.synthetic.main.fragment_song.ivSkip
+import kotlinx.android.synthetic.main.fragment_song.ivSkipPrevious
 import kotlinx.android.synthetic.main.fragment_song.ivSongImage
+import kotlinx.android.synthetic.main.fragment_song.seekBar
+import kotlinx.android.synthetic.main.fragment_song.tvCurTime
+import kotlinx.android.synthetic.main.fragment_song.tvSongDuration
 import kotlinx.android.synthetic.main.fragment_song.tvSongName
+import java.text.SimpleDateFormat
+import java.util.Locale
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -29,10 +40,47 @@ class SongFragment : Fragment(R.layout.fragment_song) {
 
     private var curPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
+    private var shouldUpdateSeekbar = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         subscribeToObservers()
+
+        ivPlayPauseDetail.setOnClickListener {
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
+
+        ivSkipPrevious.setOnClickListener {
+            mainViewModel.skipToPreviousSong()
+        }
+
+        ivSkip.setOnClickListener {
+            mainViewModel.skipToNextSong()
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    setCurPlayerTimeToTextView(progress.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                shouldUpdateSeekbar = false
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                seekBar?.let {
+                    mainViewModel.seekTo(it.progress.toLong())
+                    shouldUpdateSeekbar = true
+                }
+            }
+        })
     }
 
     private fun updateTitleAndSongImage(song: Song) {
@@ -63,5 +111,33 @@ class SongFragment : Fragment(R.layout.fragment_song) {
             curPlayingSong = it.toSong()
             updateTitleAndSongImage(curPlayingSong!!)
         }
+
+        mainViewModel.playbackState.observe(viewLifecycleOwner) {
+            playbackState = it
+            ivPlayPauseDetail.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+
+            seekBar.progress = it?.position?.toInt() ?: 0
+        }
+
+        songViewModel.curPlayerPosition.observe(viewLifecycleOwner) {
+            // 사용자가 seekbar를 탐색하고있는 동안, 즉 seekbar의 update가 발생하지 않는동안에만 progress를 업데이트해야한다.
+            if (shouldUpdateSeekbar) {
+               seekBar.progress = it.toInt()
+               setCurPlayerTimeToTextView(it)
+            }
+        }
+
+        songViewModel.curSongDuration.observe(viewLifecycleOwner) {
+            seekBar.max = it.toInt()
+            val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+            tvSongDuration.text = dateFormat.format(it)
+        }
+    }
+
+    private fun setCurPlayerTimeToTextView(ms: Long) {
+        val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+        tvCurTime.text = dateFormat.format(ms)
     }
 }
