045c308016eaf246657c414ef994a769444367e5
jiwon <kjw2000e@2beone.net>
2024-03-05 10:48:00 +0900

    playbackstate에 따른 ui 변경 playbackState, isConnected, networkError 옵져빙

--- app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.plcoding.spotifycloneyt
 
+import android.media.session.PlaybackState
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
+import com.plcoding.spotifycloneyt.Exoplayer.isPlaying
 import com.plcoding.spotifycloneyt.Exoplayer.toSong
 import com.plcoding.spotifycloneyt.adapters.SwipeSongAdapter
 import com.plcoding.spotifycloneyt.data.entities.Song
@@ -12,6 +17,8 @@ import com.plcoding.spotifycloneyt.other.Status.*
 import com.plcoding.spotifycloneyt.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.ivCurSongImage
+import kotlinx.android.synthetic.main.activity_main.ivPlayPause
+import kotlinx.android.synthetic.main.activity_main.rootLayout
 import kotlinx.android.synthetic.main.activity_main.vpSong
 import javax.inject.Inject
 
@@ -29,13 +36,32 @@ class MainActivity : AppCompatActivity() {
 
     private var curPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         subscribeToObservers()
+
         vpSong.adapter = swipeSongAdapter
+        vpSong.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                if (playbackState?.isPlaying == true) { // 재생 중인 경우 선택된 곡으로 재생 요청
+                    mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+                } else { // 재생 중이지 않을때는 현재곡으로 저장만
+                    curPlayingSong = swipeSongAdapter.songs[position]
+                }
+            }
+        })
+
+        ivPlayPause.setOnClickListener {
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
     }
 
     private fun switchViewPagerToCurrentSong(song: Song) {
@@ -72,6 +98,38 @@ class MainActivity : AppCompatActivity() {
             curPlayingSong = it.toSong()
             glide.load(curPlayingSong?.imgUrl).into(ivCurSongImage)
             switchViewPagerToCurrentSong(curPlayingSong ?: return@observe)
+        }
+
+        mainViewModel.playbackState.observe(this) {
+            playbackState = it
+            ivPlayPause.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+        }
+        mainViewModel.isConnected.observe(this) {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        rootLayout,
+                        result.message ?: "An unknown error occured",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                    else -> Unit
+                }
+            }
+        }
+
+        mainViewModel.networkError.observe(this) {
+            it?.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    ERROR -> Snackbar.make(
+                        rootLayout,
+                        result.message ?: "An unknown error occured",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                    else -> Unit
+                }
+            }
         }
     }
 }
